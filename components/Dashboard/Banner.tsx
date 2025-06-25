bcafa53197bcfbf1aa35c6d0fbf3ed877a653ba5
Dandi <dandi.juliandi@nawadata.id>
2025-06-25 07:09:19 +0700

    change Dashboard Banner component

--- components/Dashboard/Banner.tsx
+++ components/Dashboard/Banner.tsx
@@ -1,40 +1,41 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
 import colors from '@/enums/color';
 import LinearColors from '@/enums/linearColors';
-import { LinearGradient } from 'expo-linear-gradient';
-import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import BannerImage from './images/BannerImage';
 
 const Banner = () => {
   return (
-    <View style={styles.container}>
-      <BannerImage />
-      <View style={styles.overlay}>
-        <View style={styles.textContainer}>
-          <Text allowFontScaling={false} style={styles.title}>
-            BMI (Body Mass Index)
-          </Text>
-          <Text allowFontScaling={false} style={styles.subtitle}>
-            You have a normal weight
-          </Text>
-          <Pressable>
-            <LinearGradient
-              start={{ x: 1, y: 1 }}
-              end={{ x: 0, y: 0 }}
-              colors={LinearColors.PurpleLinear}
-              style={styles.button}
-            >
-              <Text allowFontScaling={false} style={styles.buttonText}>
-                View More
-              </Text>
-            </LinearGradient>
-          </Pressable>
-        </View>
-        <Text allowFontScaling={false} style={styles.bmiValue}>
-          20,1
+    <LinearGradient
+      colors={LinearColors.BlueLinear}
+      start={{ x: 1, y: 1 }}
+      end={{ x: 0, y: 0 }}
+      style={styles.container}
+    >
+      <View style={styles.textContainer}>
+        <Text allowFontScaling={false} style={styles.title}>
+          BMI (Body Mass Index)
         </Text>
+        <Text allowFontScaling={false} style={styles.subtitle}>
+          You have a normal weight
+        </Text>
+        <Pressable>
+          <LinearGradient
+            start={{ x: 1, y: 1 }}
+            end={{ x: 0, y: 0 }}
+            colors={LinearColors.PurpleLinear}
+            style={styles.button}
+          >
+            <Text allowFontScaling={false} style={styles.buttonText}>
+              View More
+            </Text>
+          </LinearGradient>
+        </Pressable>
       </View>
-    </View>
+      <Text allowFontScaling={false} style={styles.bmiValue}>
+        20,1
+      </Text>
+    </LinearGradient>
   );
 };
 
@@ -42,27 +43,24 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     height: 170,
-  },
-  overlay: {
-    position: 'absolute',
+    borderRadius: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: 300,
-    height: 150,
-    top: 10,
+    padding: 20,
+    marginBottom: 10,
   },
   textContainer: {
-    padding: 10,
     justifyContent: 'center',
   },
   title: {
     fontSize: 15,
-    fontWeight: '900',
+    fontWeight: 'bold',
     color: colors.white,
   },
   subtitle: {
     fontSize: 14,
     color: colors.white,
+    marginBottom: 20,
   },
   button: {
     width: 100,
@@ -70,19 +68,16 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 20,
   },
   buttonText: {
     fontSize: 12,
     color: colors.white,
-    fontWeight: '500',
+    fontWeight: 'medium',
   },
   bmiValue: {
-    fontSize: 14,
-    fontWeight: '900',
+    fontSize: 20,
+    fontWeight: 'bold',
     color: colors.white,
-    marginRight: 32,
-    marginTop: 45,
   },
 });
 
